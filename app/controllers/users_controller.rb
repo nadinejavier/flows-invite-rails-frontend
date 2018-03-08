c707a477fdc7595632e81db62add00e07559ef5a
Nadine <nad4ne@gmail.com>
2018-03-07 17:41:32 -0800

    cleaned unecessary comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,29 +15,12 @@ class UsersController < ApplicationController
       password: params[:password]}
     ).body
     @id = @new_user["id"]
-    # user = User.new(new_user)
-    # puts user
-    # if new_user["version"].to_i == 1
-    #   session[:user_id] = user.id
-    #   flash[:success] = 'Successfully created account!'
-    #   redirect_to '/'
-    # else
-    #   flash[:warning] = 'Invalid email or password!'
-    #   redirect_to '/'
-    # end
-    redirect_to @new_user
+    redirect_to "/users/#{@id}"
 
   end
 
   def show
      @user = User.find(params[:id])    
-      # headers:{'id' => @user.id },
-    # if @user.id != current_user.id
-    #   flash[:warning] = 'Access denied.'
-    #   redirect_to '/login'
-    # else
-    #   render 'show'
-    # end
   end
 
 end
